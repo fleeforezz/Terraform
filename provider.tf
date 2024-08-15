85991da877d91e928e6a1dcd8e7442023b010f79
Trương Minh Nhật <129656223+fleeforezz@users.noreply.github.com>
2024-08-15 18:04:52 +0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -11,6 +11,6 @@ terraform {
 provider "proxmox" {
     pm_tls_insecure = true
     pm_api_url = "https://proxmox.fleeforezz.me/api2/json"
-    pm_api_token_secret = "5d379194-1856-4e13-9cc8-0a5876509ad8"
+    pm_api_token_secret = "proxmox-token"
     pm_api_token_id = "root@pam!terraform"
 }
